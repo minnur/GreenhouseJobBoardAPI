02476b791e55be3c225c17b66ca18a81a32704bf
Minnur Yunusov <mmyunusov@gmail.com>
2015-08-26 15:51:10 -0700

    Refine example.

--- examples/application.php
+++ examples/application.php
@@ -11,8 +11,9 @@ require '../src/GreenhouseJobBoardAPI.php';
 use \GreenhouseJobBoardAPI\GreenhouseJobBoardAPI;
 
 $api_url = "https://api.greenhouse.io/v1/boards/{{CLIENT_CODE}}/embed/";
+$api_key = 'Greenhouse API Key'; // required to post application forms.
 
-$greenhouse = new GreenhouseJobBoardAPI($api_url);
+$greenhouse = new GreenhouseJobBoardAPI($api_url, $api_key);
 
 $post_data = [];
 
