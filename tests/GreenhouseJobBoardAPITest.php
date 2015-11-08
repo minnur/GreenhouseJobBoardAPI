39e9a2810243faaadd4c20da7b0766503f84956e
Minnur Yunusov <mmyunusov@gmail.com>
2015-11-08 11:45:22 -0800

    Initial unit test file. Update License. Update travis file.

---
+++ tests/GreenhouseJobBoardAPITest.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Tests for GreenhouseJobBoardAPI\GreenhouseJobBoardAPI.
+ */
+
+use GreenhouseJobBoardAPI\GreenhouseJobBoardAPI;
+use Curl\Curl;
+
+/**
+ * Tests using GreenhouseJobBoardAPI class.
+ */
+class GreenhouseJobBoardAPITest extends PHPUnit_Framework_TestCase {
+
+  /**
+   * Test multipartFinalize().
+   */
+  public function testMultipartFinalize() {
+    $this->assertEquals(TRUE, TRUE);
+  }
+
+}
